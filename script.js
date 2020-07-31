bdc6ef92a83ef588dcc060a4267642eb3cdb4b50
Mikhail Kovyorov <sb6659@yandex.ru>
2020-07-31 15:30:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//a = 5;
+//console.log(a);
+
+var number = 5;
+console.log(number);
+
+var string = "Hello!";
+console.log(string);
+
+let persone = {
+	name: "Jonh",
+	age: 25,
+	isMarried: false
+};
+console.log(persone.name);
+
+let arr = ["plum.png", "orange.jpeg", "apple.bmp"];
+console.log(arr[1]); 
+
+let incr = 10,
+	decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log("2" === 2);
+
+let isChecked = false,
+	isClose = false;
+
+console.log(isChecked || !isClose);
